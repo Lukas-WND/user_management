ff3cdcfd9dd23c52941e8faf5b23a8bf01091878
Lukas Wendel <lukas.palheta@flex-am.com.br>
2025-04-11 12:58:50 -0400

    :wrench: config: update the dockerfile and compose of frontend app

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      };
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
